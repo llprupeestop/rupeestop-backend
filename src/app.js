115695e60230feff1fb696fa7bb0d4eb6d59dd8e
Shubham <shubhamkumar7112002@gmail.com>
2025-02-28 16:54:02 +0530

    fixed2

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 // Error Handling Middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ success: false, message: "Internal Server Error" });
+  res.status(500).json({ success: false, message: "Internal Server Error!" });
 });
 
 app.use("/auth", authRouter);
